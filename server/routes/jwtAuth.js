fb1262a479d776770965764f8dfe8ff572daa66f
Mari Azh <Marzi.aghchay@gmail.com>
2023-08-03 23:56:58 +0100

    add valid info

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -1,39 +1,37 @@
 const express = require("express");
 const router = express.Router();
-
 const bcrypt = require("bcrypt");
 const pool = require("../db");
-
+const validInfo = require("../middleware/validInfo");
 const jwtGenerator = require("../utils/jwtGenerator");
 
 //1
-router.post("/register", async (req, res) => {
+router.post("/register", validInfo, async (req, res) => {
   try {
     const { email, name, password } = req.body;
 
     const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
       email,
     ]);
-//2
-     if (user.rows.length > 0) {
-       return res.status(401).json("User already exist!");
-     }
+    //2
+    if (user.rows.length > 0) {
+      return res.status(401).json("User already exist!");
+    }
 
     //3
     const salt = await bcrypt.genSalt(10);
     const bcryptPassword = await bcrypt.hash(password, salt);
 
-//4
-     let newUser = await pool.query(
-       "INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *",
-       [name, email, bcryptPassword]
-     );
+    //4
+    let newUser = await pool.query(
+      "INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *",
+      [name, email, bcryptPassword]
+    );
 
-//5
+    //5
     const token = jwtGenerator(newUser.rows[0].user_id);
 
-    res.json({token});
-
+    res.json({ token });
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
@@ -47,7 +45,7 @@ router.post("/register", async (req, res) => {
 
 //login route
 
-router.post("/login",  async (req, res) => {
+router.post("/login", validInfo, async (req, res) => {
   //1
   const { email, password } = req.body;
 
@@ -57,31 +55,29 @@ router.post("/login",  async (req, res) => {
     ]);
 
     //2
- 
 
-      if (user.rows.length === 0) {
-        return res.status(401).json("Invalid Credential");
-      }
+    if (user.rows.length === 0) {
+      return res.status(401).json("Invalid Credential");
+    }
     //3
-   const validPassword = await bcrypt.compare(
-     password,
-     user.rows[0].user_password
-   );
-//    console.log(validPassword)
+    const validPassword = await bcrypt.compare(
+      password,
+      user.rows[0].user_password
+    );
+    //    console.log(validPassword)
 
     if (!validPassword) {
       return res.status(401).json("Invalid Credential");
     }
 
     //4
-        const token = jwtGenerator(user.rows[0].user_id);
+    const token = jwtGenerator(user.rows[0].user_id);
 
-        res.json({ token });  
-          
+    res.json({ token });
   } catch (err) {
     console.error(err.message);
   }
-})
+});
 
 
 
