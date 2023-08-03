145b048469424af87baa83b0dcd1f73fa1f3f2eb
Mari Azh <Marzi.aghchay@gmail.com>
2023-08-03 16:26:39 +0100

    jwtGenerator
    token

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const pool = require("../db");
 
+const jwtGenerator = require("../utils/jwtGenerator");
 
+//1
 router.post("/register", async (req, res) => {
   try {
     const { email, name, password } = req.body;
@@ -12,25 +14,25 @@ router.post("/register", async (req, res) => {
     const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
       email,
     ]);
-
+//2
      if (user.rows.length > 0) {
        return res.status(401).json("User already exist!");
      }
 
-    
+    //3
     const salt = await bcrypt.genSalt(10);
     const bcryptPassword = await bcrypt.hash(password, salt);
 
-
+//4
      let newUser = await pool.query(
        "INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *",
        [name, email, bcryptPassword]
      );
 
+//5
+    const token = jwtGenerator(newUser.rows[0].user_id);
 
-
-
-    res.json(newUser.rows[0]);
+    res.json({token});
 
   } catch (err) {
     console.error(err.message);
